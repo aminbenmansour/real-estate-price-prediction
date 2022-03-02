f706dba5c952fe9beaf665aa32601e0379b06ebb
aminbenmansour <amin.benmansour.10@gmail.com>
2022-03-02 21:33:53 +0100

    get locations endpoint

--- server/main.py
+++ server/main.py
@@ -13,3 +13,8 @@ class House(BaseModel):
 @app.on_event("startup")
 async def startup_event():
     utils.load_saved_artifacts()
+
+
+@app.get("/get-location-names")
+def get_location_names():
+    utils.get_location_names()
