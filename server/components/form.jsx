326204132f08249f296d9e58aaa1fd314f6df7d1
aminbenmansour <amin.benmansour.10@gmail.com>
2022-03-05 23:51:13 +0100

    create form component

---
+++ server/components/form.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import locations from '../data/locations.json';
+import { api } from "../App";
+
+
+let locs = locations["locs"];
+
+function Form() {
+    const [price, setPrice] = useState(-1)
+    const {register, handleSubmit} = useForm();
+
+    let prediction = <h2>{price}</h2>;
+
+    const submitForm = async (data) => {
+
+        await api.post('predict-home-price', data)
+                            .then(function(response) {
+                                setPrice(response.data['estimated_price']);
+                            })
+    }
+
+
+    return (
+        <div className="Form">
+            <div className="title">                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  <form onSubmit={handleSubmit(submitForm)}>
+                    <input type="number" placeholder="rooms" {...register('rooms', {required: true})} />
+                    <input type="number" placeholder="sqft" {...register('sqft')} />
+                    <input type="number" placeholder="baths" {...register('baths')} />
+                    <select {...register('location')}>
+                        {
+                            locs.map((loc) => 
+                                <option key={loc} value={loc}>{loc}</option>
+                            )
+                        }
+                        
+                    </select>
+                    <input type="submit" value="submit"/>
+                </form>
+            </div>
+            
+        {price > 0 && prediction}
+        </div>
+
+    );
+}
+
+export default Form 
