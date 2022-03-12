4fbe8fa81f4cfcd661854d0d19962706fd127157
aminbenmansour <amin.benmansour.10@gmail.com>
2022-03-13 00:43:02 +0100

    fixing cors issue

--- server/main.py
+++ server/main.py
@@ -41,8 +41,7 @@ def predict_home_price(data: House):
     response = {
         'estimated_price': utils.get_estimated_price(location,total_sqft,bhk,bath)
     }
-    
-    response.headers.add('Access-Control-Allow-Origin', '*')
+
     return response
 
 
