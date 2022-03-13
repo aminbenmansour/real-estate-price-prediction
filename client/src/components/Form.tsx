bc3af3f5d491b72e135ec7571198d5d5a51666b5
aminbenmansour <amin.benmansour.10@gmail.com>
2022-03-13 01:21:14 +0100

    final touch

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -3,44 +3,71 @@ import { useForm } from "react-hook-form";
 import locations from '../data/locations.json';
 import { api } from "../App";
 
+import Card from "@mui/material/Card";
+import Input from "@mui/material/Input";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import InputLabel from "@mui/material/InputLabel";
+import { CardActions, CardContent, Grid, Typography } from "@mui/material";
+
 
 let locs = locations["locs"];
 
 function Form() {
     const [price, setPrice] = useState(-1)
-    const {register, handleSubmit} = useForm();
+    const { register, handleSubmit } = useForm();
 
-    let prediction = <h2>{price}</h2>;
+    let prediction = <div>
+        <Typography>
+            <h2>The estimated price is: {-price} rupees</h2>
+        </Typography>
+    </div>;
 
     const submitForm = async (data: any) => {
 
         await api.post('predict-home-price', data)
-                            .then(function(response) {
-                                setPrice(response.data['estimated_price']);
-                            })
+            .then(function (response) {
+                setPrice(response.data['estimated_price']);
+            })
     }
 
 
     return (
         <div className="Form">
-            <div className="title">   
-                <form onSubmit={handleSubmit(submitForm)}>
-                    <input type="number" placeholder="rooms" {...register('rooms', {required: true})} />
-                    <input type="number" placeholder="sqft" {...register('sqft')} />
-                    <input type="number" placeholder="baths" {...register('baths')} />
-                    <select {...register('location')}>
-                        {
-                            locs.map((loc) => 
-                                <option key={loc} value={loc}>{loc}</option>
-                            )
-                        }
-                        
-                    </select>
-                    <input type="submit" value="submit"/>
-                </form>
+            <div className="title">
+                <Grid container justifyContent="center">
+                    <Card variant="outlined" sx={{ width: 1/4 }}>
+                        <form onSubmit={handleSubmit(submitForm)}>
+                            <CardContent>
+                                <Input sx={{ width: 3/4 }} type="number" placeholder="Type the number of rooms" {...register('rooms', { required: true })} required /> <br />
+                                <Input sx={{ width: 3/4 }} type="number" placeholder="Type square feet" {...register('sqft')} required /> <br />
+                                <Input sx={{ width: 3/4 }} type="number" placeholder="Type the number of baths" {...register('baths')} required /> <br />
+
+                                <InputLabel id="select-locations">Locations</InputLabel>
+                                <Select {...register('location')}
+                                    sx={{ width: 3/4 }} 
+                                    labelId="select-locations"
+                                    label="Choose location"
+                                    defaultValue="other">
+                                    {
+                                        locs.map((loc) =>
+                                            <MenuItem key={loc} value={loc}>{loc}</MenuItem>
+                                        )
+                                    }
+
+                                </Select> <br />
+                            </CardContent>
+
+                            <CardActions>
+                                <Button variant="contained" color="success" type="submit" value="submit">Submit</Button>
+                            </CardActions>
+                        </form>
+                    </Card>
+                </Grid>
             </div>
-            
-        {price > 0 && prediction}
+
+            {prediction}
         </div>
 
     );
